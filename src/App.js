e09e4f9699a13367b64a37d777d379f2e230f0eb
Kenza <kenzabaccar6@gmail.com>
2024-05-01 20:29:19 +0200

    Adding routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { Home } from './pages/Home';
 import 'tailwindcss/tailwind.css';
+import DashboardIssues from './pages/Dashboards/DashboardsIssues';
+import DashboardsProject from './pages/Dashboards/DashboardsProject';
+import DashboardsSprintFinish from './pages/Dashboards/DashboardsSprintFinish';
+import DashboardsSprintProg from './pages/Dashboards/DashboardsSprintProg';
 import Landing from './pages/Landing';
 import Login from './pages/login';
 function App() {
 	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/login" element={<Login />} />
-		</Routes>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/DashboardsIssues" element={<DashboardIssues />}/>
+				<Route path="/DashboardsProject" element={<DashboardsProject />}/>
+				<Route path="/DashboardsSprintFinish" element={<DashboardsSprintFinish />}/>
+				<Route path="/DashboardsSprintProg" element={<DashboardsSprintProg />}/>
+			</Routes>
 	);
 }
 
