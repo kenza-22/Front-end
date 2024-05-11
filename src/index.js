9ca814a2a9cd54e7e1191a49bdeb87b8782768bf
Kenza <kenzabaccar6@gmail.com>
2024-05-11 12:16:19 +0200

    update file index

--- src/index.js
+++ src/index.js
@@ -1,50 +1,43 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "tailwindcss/tailwind.css";
-import { BrowserRouter } from "react-router-dom";
-
-import App from "./App";
-
-import { PublicClientApplication, EventType } from "@azure/msal-browser";
-
-import {
-  MsalProvider,
-  AuthenticatedTemplate,
-  UnauthenticatedTemplate,
-} from "@azure/msal-react";
-import Landing from "./pages/Landing";
-import { HomeLayout } from "./pages/Home";
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'tailwindcss/tailwind.css';
+import { BrowserRouter } from 'react-router-dom';
+ 
+import App from './App';
+ 
+import { PublicClientApplication, EventType } from '@azure/msal-browser';
+ 
+import { MsalProvider, AuthenticatedTemplate, UnauthenticatedTemplate } from '@azure/msal-react';
+import Landing from './pages/Landing';
+import { HomeLayout } from './pages/Home';
+ 
 const pca = new PublicClientApplication({
-  auth: {
-    clientId: "716fa805-d117-44b8-8d9a-f6443efba185",
-    authority:
-      "https://login.microsoftonline.com/fe013626-9e98-4cbe-b49e-c5848b622c2d/oauth2/v2.0/authorize",
-    redirectUri: "http://localhost:3000",
-  },
+    auth: {
+        clientId: '716fa805-d117-44b8-8d9a-f6443efba185',
+        authority: 'https://login.microsoftonline.com/fe013626-9e98-4cbe-b49e-c5848b622c2d/oauth2/v2.0/authorize',
+        redirectUri: 'http://localhost:3000'
+    }
 });
-
-pca.addEventCallback((event) => {
-  if (event.eventType === EventType.LOGIN_SUCCESS) {
-    console.log(event);
-    pca.setActiveAccount(event.payload.account);
-  }
+ 
+pca.addEventCallback(event => {
+    if (event.eventType === EventType.LOGIN_SUCCESS) {
+        console.log(event);
+        pca.setActiveAccount(event.payload.account);
+    }
 });
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+ 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <MsalProvider instance={pca}>
-        <AuthenticatedTemplate>
-          <HomeLayout>
-            <App />
-          </HomeLayout>
-        </AuthenticatedTemplate>
-        <UnauthenticatedTemplate>
-          <Landing />
-        </UnauthenticatedTemplate>
-      </MsalProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <MsalProvider instance={pca}>
+                <AuthenticatedTemplate>
+                    <App />
+                </AuthenticatedTemplate>
+                <UnauthenticatedTemplate>
+                    <Landing />
+                </UnauthenticatedTemplate>
+            </MsalProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
