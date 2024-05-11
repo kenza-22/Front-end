96da0ff1a6bd425ba7256c678c9ab0c507befe70
Kenza <kenzabaccar6@gmail.com>
2024-05-11 12:27:12 +0200

    Doughnut chart

--- src/components/Charts/DoughnutCharts/DoughnutChart.jsx
+++ src/components/Charts/DoughnutCharts/DoughnutChart.jsx
@@ -4,40 +4,92 @@ import axios from "axios";
 
 function DoughnutChart({ selected }) {
   const [storyPointData, setStoryPointData] = useState([]);
-
+  const [ticketTypes, setTicketTypes] = useState([]);
+  const [ticketCounts, setTicketCounts] = useState([]);
   useEffect(() => {
     if (selected) {
       axios
         .get(`http://localhost:5000/tickets/${selected}/kpi/complexity-by-type`)
         .then((res) => {
-          console.log("Story points par type:", res.data);
-          setStoryPointData(res.data);
+          console.log("Complexité par type:", res.data);
+          const types = res.data.map((item) => item._id);
+          const counts = res.data.map((item) => item.totalStoryPoints);
+          setTicketTypes(types);
+          setTicketCounts(counts);
         })
         .catch((err) => console.log(err));
     }
   }, [selected]);
-
+  const customColors = ticketTypes.map((type) => {
+    if (type === "Bug") {
+      return 'rgba(255, 0, 0, 0.5)'; 
+      
+    } else {
+      if(type === "Sous-tâche")
+      return 'rgba(176, 224, 230, 1)'; 
+      else{
+        if (type === "Story")
+        return 'rgba(216, 191, 216, 1)'
+        else{
+          if (type === "Tâche")
+          return 'rgba(176, 196, 222, 1)'
+          else{
+            if (type === "Epic")
+            return 'rgba(138, 43, 226, 1)'
+            else{
+              if (type === "Support")
+              return 'rgba(255, 140, 0, 1)'
+              else{
+                if (type === "Tâche technique")
+                return 'rgba(100, 149, 237, 1)'
+                else{
+                  if (type === "Test task")
+                  return 'rgba(255, 255, 0, 1)'
+                  else{
+                    if (type === "Testing task")
+                    return 'rgba(255, 215, 0, 1)'
+                    else{
+                      if (type === "Bogue")
+                      return 'rgba(205, 92, 92, 1)'
+                      else{
+                        if (type === "Bug Sub Task")
+                        return 'rgba(255, 67, 67, 1)'
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      
+    }
+  });
   const dataDoughnut = {
-    labels: storyPointData.map((item) => item._id),
+    labels: ticketTypes,
     datasets: [
       {
-        label: "Nombre de Story Points",
-        data: storyPointData.map((item) => item.totalStoryPoints),
-        backgroundColor: [
-          "rgba(80, 55, 90, 0.8)",
-          "rgba(100, 30, 40, 0.8)",
-          "rgba(20, 255, 6, 0.8)",
-          "rgba(250, 70, 100, 0.8)",
-          "rgba(255, 10, 150, 0.8)",
-          // Ajoutez plus de couleurs si nécessaire
-        ],
+        data: ticketCounts,
+        backgroundColor: customColors,
+       
       },
     ],
   };
 
   return (
     <div style={{ width: "300px" }}>
-      <Doughnut data={dataDoughnut} />
+      <Doughnut 
+      data={dataDoughnut} 
+      options={{
+        plugins: {
+          title: {
+            display: true,
+            text: "Complexité des tickets Par type",
+          },
+        },
+      }}
+      />
     </div>
   );
 }
